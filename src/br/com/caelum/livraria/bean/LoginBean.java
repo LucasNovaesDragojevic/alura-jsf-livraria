ef1436baf3e7c85dc00b31c7b1d3df95fa80f679
Lucas Novaes Dragojevic <lucas.dragojevic@gmail.com>
2020-06-29 18:46:25 -0300

    Autorização com o PhaseListener

--- src/br/com/caelum/livraria/bean/LoginBean.java
+++ src/br/com/caelum/livraria/bean/LoginBean.java
@@ -2,6 +2,7 @@ package br.com.caelum.livraria.bean;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import br.com.caelum.livraria.dao.UsuarioDao;
 import br.com.caelum.livraria.modelo.Usuario;
@@ -21,6 +22,8 @@ public class LoginBean {
 		System.out.println("Efetuando login: " + this.usuario.getEmail());
 		
 		if ((new UsuarioDao()).existe(this.usuario)) {
+			FacesContext context = FacesContext.getCurrentInstance();
+			context.getExternalContext().getSessionMap().put("usuarioLogado", this.usuario);
 			return new RedirectView("livro");
 		}
 		return new RedirectView("login");
